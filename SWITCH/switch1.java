70c5953a63893a29735bc6f456cc7f0d5c786263
Ajey23 <ajaysarode2121@gmail.com>
2022-06-09 15:37:09 +0530

    master

---
+++ SWITCH/switch1.java
@@ -0,0 +1,29 @@
+//Switch case
+//program print week days using switch case
+
+class switch1
+{
+    public static void main(String arr[])
+    {
+        String day ="Mon";
+
+        switch (day)
+        {
+            case "Mon":
+                System.out.println("hey it is Monday");
+                break;
+
+                case "Tue":
+                    System.out.println("hey it is Tuesday");
+                    break;
+
+                    case "Wed":
+                        System.out.println("hey it is Wednesday");
+                        break;
+
+                        default:
+                            System.out.println("Invalid day");
+        }
+    }
+}
+
